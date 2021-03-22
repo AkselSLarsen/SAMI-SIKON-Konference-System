2ca43e05a646f12655481d04c3c4786a10759f2a
Sebastian Løgtholt Westergaard <seba255j@edu.easj.dk>
2021-03-22 13:47:36 +0100

    rbrwbwerb

--- SAMI-SIKON/Startup.cs
+++ SAMI-SIKON/Startup.cs
@@ -18,6 +18,7 @@ namespace SAMI_SIKON {
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // egmwglwwkweobswopwb
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
         }
