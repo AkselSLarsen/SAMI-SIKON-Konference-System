e3d9b7ce839f2ea607bd0b7bb8e41662e96764b0
Sebastian Løgtholt Westergaard <seba255j@edu.easj.dk>
2021-04-26 11:24:29 +0200

    Iuser admin participant

---
+++ SAMI-SIKON/Interfaces/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAMI_SIKON.Interfaces
+{
+    public interface IUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Id { get; set; }
+
+        public void Login();
+    }
+}
---
+++ SAMI-SIKON/Model/Administrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SAMI_SIKON.Interfaces;
+
+namespace SAMI_SIKON.Model
+{
+    public class Administrator : IUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Id { get; set; }
+
+        public void Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
