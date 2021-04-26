101f9ca546c2d7831b734550280f8c904435900e
AkselSLarsen <akse2669@edu.easj.dk>
2021-04-26 09:42:51 +0200

    Created ICatalogue inside Interfaces folder and added Model folder

---
+++ SAMI-SIKON/Interfaces/ICatalogue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAMI_SIKON.Interfaces {
+    public interface ICatalogue<T> {
+
+        void Create(T t);
+
+        T Read(int i);
+
+        void Update(T pre, T post);
+
+        T Delete(int i);
+
+        List<T> ReadAll();
+
+    }
+}
