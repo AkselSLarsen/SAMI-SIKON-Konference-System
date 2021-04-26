57e9792ed94f854c40489deb67dfdef934e3e8b8
AkselSLarsen <akse2669@edu.easj.dk>
2021-04-26 14:01:21 +0200

    Changed the way the Catalogues work.

--- SAMI-SIKON/Interfaces/ICatalogue.cs
+++ SAMI-SIKON/Interfaces/ICatalogue.cs
@@ -6,15 +6,84 @@ using System.Threading.Tasks;
 namespace SAMI_SIKON.Interfaces {
     public interface ICatalogue<T> {
 
-        void Create(T t);
+        /// <summary>
+        /// Should contain the name of the database table.
+        /// </summary>
+        string RelationalName { get; protected set; }
+        /// <summary>
+        /// Should contain the name(s) of the key(s) of the database table.
+        /// </summary>
+        string[] RelationalKeys { get; protected set; }
+        /// <summary>
+        /// Should contain the name(s) of the non-prime attribute(s) of the database table. 
+        /// </summary>
+        string[] RelationalAttributes { get; protected set; }
 
-        T Read(int i);
-
-        void Update(T pre, T post);
-
-        T Delete(int i);
-
-        List<T> ReadAll();
+        /// <summary>
+        /// Retrieves all elements of the table with the name contained by the RelationalName property.
+        /// Elements possess all keys and attributes contained by the RelationalKeys and RelationalAttributes properties.
+        /// </summary>
+        /// <returns>A list of all elements in the table with the name contained in RelationalName</returns>
+        Task<List<T>> GetAllItems();
+        /// <summary>
+        /// Retrieves all elements of the table where the key with the given number is equal to the given key.
+        /// Elements possess all keys and attributes contained by the RelationalKeys and RelationalAttributes properties.
+        /// </summary>
+        /// <param name="keyNr">The placement of the keyname in the array returned by RelationalKeys</param>
+        /// <param name="key">The value the key should have for the element to be returned</param>
+        /// <returns>A list of all elements with the specified key</returns>
+        Task<List<T>> GetItemsWithKey(int keyNr, object key);
+        /// <summary>
+        /// Retrieves all elements of the table where the attribute with the given number is equal to the given attribute.
+        /// Elements possess all keys and attributes contained by the RelationalKeys and RelationalAttributes properties.
+        /// </summary>
+        /// <param name="attributeNr">The placement of the attributename in the array returned by RelationalAttributes</param>
+        /// <param name="attribute">The value the attribute should have for the element to be returned</param>
+        /// <returns>A list of all elements with the specified attribute</returns>
+        Task<List<T>> GetItemsWithAttribute(int attributeNr, object attribute);
+        /// <summary>
+        /// Retrieves all elements of the table where the key with the given number contains the given key.
+        /// Elements possess all keys and attributes contained by the RelationalKeys and RelationalAttributes properties.
+        /// </summary>
+        /// <param name="keyNr">The placement of the keyname in the array returned by RelationalKeys</param>
+        /// <param name="key">The value the key should contain for the element to be returned</param>
+        /// <returns>A list of all elements with the key containing the specified key</returns>
+        Task<List<T>> GetItemsWithKeyLike(int keyNr, string key);
+        /// <summary>
+        /// Retrieves all elements of the table where the attribute with the given number contains the given attribute.
+        /// Elements possess all keys and attributes contained by the RelationalKeys and RelationalAttributes properties.
+        /// </summary>
+        /// <param name="attributeNr">The placement of the attributename in the array returned by RelationalAttributes</param>
+        /// <param name="attribute">The value the attribute should contain for the element to be returned</param>
+        /// <returns>A list of all elements with the attribute containing the specified attribute</returns>
+        Task<List<T>> GetItemsWithAttributeLike(int attributeNr, string attribute);
+        /// <summary>
+        /// Retrieves the element, of the table with the name returned by the RelationalName property, with the given ids.
+        /// the element possess all keys and attributes contained by the RelationalKeys and RelationalAttributes properties.
+        /// </summary>
+        /// <param name="ids">The key values of the desired elements. This array should have the same length as the one contained by RelationalKeys</param>
+        /// <returns>The element with the keys equal to the given ids. If no element has the given key combination, then it returns null</returns>
+        Task<T> GetItem(int[] ids);
+        /// <summary>
+        /// Inserts a new element with the properties of the item into the table with the name contained by the RelationalName property.
+        /// </summary>
+        /// <param name="t">The element to insert</param>
+        /// <returns>Returns if the opperation successfully inserted the element</returns>
+        Task<bool> CreateItem(T t);
+        /// <summary>
+        /// Updates the element with keys equal to the given ids, from the table with the name contained by the RelationalName property.
+        /// It is possible to change the keys of the element, by having the item contain different keys than the given ids.
+        /// </summary>
+        /// <param name="t">The element to insert</param>
+        /// <param name="ids">The key combination of the element to delete. This array should have the same length as the one contained by RelationalKeys</param>
+        /// <returns>Returns if the opperation successfully updated the element</returns>
+        Task<bool> UpdateItem(T t, int[] ids);
+        /// <summary>
+        /// Deletes the element with keys equal to the given ids.
+        /// </summary>
+        /// <param name="ids">The key combination of the element to delete. This array should have the same length as the one contained by RelationalKeys</param>
+        /// <returns>The deleted element if any, otherwise null</returns>
+        Task<T> DeleteItem(int[] ids);
 
     }
 }
---
+++ SAMI-SIKON/Services/Catalogue.cs
@@ -0,0 +1,203 @@
+﻿using SAMI_SIKON.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAMI_SIKON.Services {
+    public abstract class Catalogue<T> : ICatalogue<T> {
+
+        /// <summary>
+        /// The connection address to the underlying relational database in string format.
+        /// </summary>
+        protected static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HotelDbtest2;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        /// <summary>
+        /// The name of the table in the relational database in string format.
+        /// </summary>
+        protected string _relationalName;
+        /// <summary>
+        /// The name of the keys in the table in the relational database in string format.
+        /// </summary>
+        protected string[] _relationalKeys;
+        /// <summary>
+        /// The name of the non-prime attributes in the table in the relational database in string format.
+        /// </summary>
+        protected string[] _relationalAttributes;
+
+        public Catalogue(string relationalName, string[] relationalKeys, string[] relationalAttributes) {
+            _relationalName = relationalName;
+            _relationalKeys = relationalKeys;
+            _relationalAttributes = relationalAttributes;
+        }
+
+        string ICatalogue<T>.RelationalName {
+            get { return _relationalName; }
+            set { _relationalName = value; }
+        }
+        string[] ICatalogue<T>.RelationalKeys {
+            get { return _relationalKeys; }
+            set { _relationalKeys = value; }
+        }
+        string[] ICatalogue<T>.RelationalAttributes {
+            get { return _relationalAttributes; }
+            set { _relationalAttributes = value; }
+        }
+
+        /// <summary>
+        /// Contains an SQL quary that retrieves all elements from the table with the name contained by RelationalName as a string.
+        /// </summary>
+        protected string SQLGetAll {
+            get {
+                string re = $"SELECT * FROM {_relationalName};";
+
+                return re;
+            }
+        }
+        /// <summary>
+        /// Creates an SQL quary that retrieves all elements where the key of the given index number has the given value.
+        /// </summary>
+        /// <param name="keyNr">The index number of the key. Must be a non-negative integer less than the length of the array contained by RelationalKeys</param>
+        /// <param name="value">The value the key should have for the element to be returned</param>
+        /// <returns>An SQL statement in string format that retrieves all elements with the key of the given number equal to the given value</returns>
+        protected string SQLGetFromKey(int keyNr, string value) {
+            string re = $"SELECT * FROM {_relationalName} WHERE {_relationalKeys[keyNr]} = {value};";
+
+            return re;
+        }
+
+        /// <summary>
+        /// Creates an SQL quary that retrieves all elements where the attribute of the given index number has the given value.
+        /// </summary>
+        /// <param name="attributeNr">The index number of the attribute. Must be a non-negative integer less than the length of the array contained by RelationalAttributes</param>
+        /// <param name="value">The value the attribute should have for the element to be returned</param>
+        /// <returns>An SQL statement in string format that retrieves all elements with the attribute of the given number equal to the given value</returns>
+        protected string SQLGetFromAtttribute(int attributeNr, string value) {
+            string re = $"SELECT * FROM {_relationalName} WHERE {_relationalAttributes[attributeNr]} = {value};";
+
+            return re;
+        }
+        /// <summary>
+        /// Creates an SQL quary that retrieves all elements where the key of the given index number is like the given value.
+        /// </summary>
+        /// <param name="keyNr">The index number of the key. Must be a non-negative integer less than the length of the array contained by RelationalKeys</param>
+        /// <param name="value">The value the key should be like for the element to be returned</param>
+        /// <returns>An SQL statement in string format that retrieves all elements with a key of the given number that is like the given value</returns>
+        protected string SQLGetLikeKey(int keyNr, string value) {
+            string re = $"SELECT * FROM {_relationalName} WHERE {_relationalKeys[keyNr]} LIKE \'%{value}%\';";
+
+            return re;
+        }
+        /// <summary>
+        /// Creates an SQL quary that retrieves all elements where the attribute of the given index number is like the given value.
+        /// </summary>
+        /// <param name="attributeNr">The index number of the attribute. Must be a non-negative integer less than the length of the array contained by RelationalAttributes</param>
+        /// <param name="value">The value the attribute should be like for the element to be returned</param>
+        /// <returns>An SQL statement in string format that retrieves all elements with the attribute of the given number that is like the given value</returns>
+        protected string SQLGetLikeAtttribute(int attributeNr, string value) {
+            string re = $"SELECT * FROM {_relationalName} WHERE {_relationalAttributes[attributeNr]} LIKE \'%{value}%\';";
+
+            return re;
+        }
+        /// <summary>
+        /// Contains an SQL quary that retrieves the element with a specific set of keys from the table with the name contained by RelationalName as a string.
+        /// </summary>
+        protected string SQLGet {
+            get {
+                string re = $"SELECT * FROM {_relationalName} WHERE ";
+                for (int i = 0; i < _relationalKeys.Length; i++) {
+                    re += _relationalKeys[i] + " = @" + _relationalKeys[i];
+
+                    if (i < _relationalKeys.Length - 1) {
+                        re += " AND ";
+                    }
+                }
+                re += ";";
+
+                return re;
+            }
+        }
+        /// <summary>
+        /// Contains an SQL quary that inserts an element with a specific set of keys and attributes into the table with the name contained by RelationalName as a string.
+        /// </summary>
+        protected string SQLInsert {
+            get {
+                string re = $"INSERT INTO {_relationalName} (";
+                foreach (string s in _relationalKeys) {
+                    re += s + ",";
+                }
+                foreach (string s in _relationalAttributes) {
+                    re += s + ",";
+                }
+                re = re.Remove(re.Length - 1);
+
+                re += ") VALUES(";
+                foreach (string s in _relationalKeys) {
+                    re += "@" + s + ",";
+                }
+                foreach (string s in _relationalAttributes) {
+                    re += "@" + s + ",";
+                }
+                re = re.Remove(re.Length - 1);
+
+                re += ");";
+
+                return re;
+            }
+        }
+        /// <summary>
+        /// Contains an SQL quary that deletes the element with a specific set of keys from the table with the name contained by RelationalName as a string.
+        /// </summary>
+        protected string SQLDelete {
+            get {
+                string re = $"DELETE FROM {_relationalName} WHERE ";
+                for (int i = 0; i < _relationalKeys.Length; i++) {
+                    re += _relationalKeys[i] + " = @" + _relationalKeys[i];
+
+                    if (i < _relationalKeys.Length - 1) {
+                        re += " AND ";
+                    }
+                }
+                re += ";";
+
+                return re;
+            }
+        }
+        /// <summary>
+        /// Contains an SQL quary that updates the element with a specific set of keys from the table with the name contained by RelationalName as a string.
+        /// </summary>
+        protected string SQLUpdate {
+            get {
+                string re = $"UPDATE {_relationalName} SET ";
+                foreach (string s in _relationalKeys) {
+                    re += s + " = @" + s + ",";
+                }
+                foreach (string s in _relationalAttributes) {
+                    re += s + " = @" + s + ",";
+                }
+                re = re.Remove(re.Length - 1);
+
+                re += " WHERE ";
+                for (int i = 0; i < _relationalKeys.Length; i++) {
+                    re += _relationalKeys[i] + " = @To_Update_" + i;
+
+                    if (i < _relationalKeys.Length - 1) {
+                        re += " AND ";
+                    }
+                }
+                re += ";";
+
+                return re;
+            }
+        }
+
+        public abstract Task<List<T>> GetAllItems();
+        public abstract Task<List<T>> GetItemsWithKey(int keyNr, object key);
+        public abstract Task<List<T>> GetItemsWithAttribute(int attributeNr, object attribute);
+        public abstract Task<List<T>> GetItemsWithKeyLike(int keyNr, string key);
+        public abstract Task<List<T>> GetItemsWithAttributeLike(int attributeNr, string attribute);
+        public abstract Task<T> GetItem(int[] ids);
+        public abstract Task<bool> CreateItem(T t);
+        public abstract Task<bool> UpdateItem(T t, int[] ids);
+        public abstract Task<T> DeleteItem(int[] ids);
+    }
+}
