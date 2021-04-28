812fd38a72c34e8d3f8b5f4c7461cc6be02d46df
Mikkel Vestergaard-Jensen <mikk23f8@edu.easj.dk>
2021-04-28 10:26:43 +0200

    added event, room class. unfinished

---
+++ SAMI-SIKON/Model/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAMI_SIKON.Model
+{
+    public class Event
+    {
+        private int room;
+        private int duration;
+        private int bookedSeats;
+        public int SeatsLeft;
+        public DateTime StartTime;
+        public DateTime StopTime;
+        public string Description;
+        public string Name;
+        public string Theme;
+        private bool seatTaken; 
+        public List<Participant> Speaker;
+
+
+    }
+}
