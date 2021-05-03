e8cd10017aa9825aebe70e4dc249f8a244d84ee4
AkselSLarsen <akse2669@edu.easj.dk>
2021-05-03 12:33:38 +0200

    Changed Room around a bit. Makeing instance fields into properties when possible.

--- SAMI-SIKON/Model/Room.cs
+++ SAMI-SIKON/Model/Room.cs
@@ -7,18 +7,35 @@ namespace SAMI_SIKON.Model
 {
     public class Room
     {
-        public int Seats;
-        public List<List<Char>> Layout;
-        public int Height;
-        public int Width;
+        private static char SeatSymbol = 'S'; //should maybe be public, if you need it in some other class feel free to make it so.
 
-        public Room(int seats, int height, int width, List<List<char>> layout)
+
+        public int Seats;
+        private List<List<char>> _layout;
+
+        public Room(List<List<char>> layout)
         {
-            Seats = seats;
             Layout = layout;
-            Height = height;
-            Width = width;
         }
+
+        public List<List<char>> Layout {
+            get { return _layout; }
+            set {
+                _layout = value;
+
+                Seats = 0;
+                foreach(List<char> cs in _layout) {
+                    foreach(char c in cs) {
+                        if(c == SeatSymbol) {
+                            Seats++;
+                        }
+                    }
+                }
+            }
+        }
+
+        public int Height { get { return Layout.Count; } }
+        public int Width { get { return Layout[0].Count; } }
 
         public int FindSeat(int x, int y)
         {
