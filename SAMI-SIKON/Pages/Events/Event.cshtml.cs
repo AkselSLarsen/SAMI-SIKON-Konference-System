1c9bf4da02df8ae7f5b0812ce535f8067766986f
AkselSLarsen <akse2669@edu.easj.dk>
2021-05-06 14:23:31 +0200

    Began refactoring the Event class, which causes all sorts of trouble.

--- SAMI-SIKON/Pages/Events/Event.cshtml.cs
+++ SAMI-SIKON/Pages/Events/Event.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SAMI_SIKON.Interfaces;
 using SAMI_SIKON.Model;
+using System;
+using System.Threading.Tasks;
 
 namespace SAMI_SIKON.Pages.Events {
     public class EventModel : PageModel {
@@ -12,30 +14,23 @@ namespace SAMI_SIKON.Pages.Events {
         public string PictureAlt { get; set; }
         public int Seats { get; set; }
 
-        public ICatalogue<Event> Catalogue { get; set; }
+        public ICatalogue<Event> EventCatalogue { get; set; }
 
-        public EventModel(ICatalogue<Event> catalogue) {
-            Catalogue = catalogue;
-            
+        public EventModel(ICatalogue<Event> eventCatalogue) {
+            EventCatalogue = eventCatalogue;
         }
 
-        /// <summary>
-        /// temp, please delete
-        /// </summary>
-        public void OnGet() {
+        public async Task OnGetAsync(int id) {
+            //Event evt = await EventCatalogue.GetItem(new int[] { id });
+            Event evt = new Event(1, 1, new System.Collections.Generic.List<Participant>(), DateTime.Now, "Lorem Ipsum", "Event Name Here", "Theme", 30, new bool[] { false, false, true});
 
-
-            Name = "Test Event";
-            Theme = "Testing of algorithmical logic and aesthetically designed pages";
-            Description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.";
-            PictureSrc = "/pictures/Auditorium.jpeg";
+            Name = evt.Name;
+            Description = evt.Description;
+            Theme = evt.Theme;
+            PictureSrc = "../pictures/Auditorium.jpeg";
             PictureAlt = "Auditorium";
-            Seats = 123;
+            //Seats = evt.SeatsLeft;
+            Seats = 3;
         }
-
-
-        //public void OnGet(int id) {
-
-        //}
     }
 }
